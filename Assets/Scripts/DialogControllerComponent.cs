208db390ad372a2e46b77664212654ce0780b436
Slashscreen <SlashScreen@users.noreply.github.com>
2020-12-07 12:40:59 -0800

    ok git come on

---
+++ Assets/Scripts/DialogControllerComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class DialogControllerComponent : Singleton<DialogControllerComponent>
+{
+    
+    public DialogueRunner dialogRunner;
+
+    void Start()
+    {
+        Show();
+    }
+
+    public void Show(){
+        gameObject.SetActive(true);
+    }
+
+    public void Hide(){
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+    public string yarnStartNode;
+    public YarnProgram yarnDialog;
+    public DialogControllerComponent dialog;
+
+    private void Start()
+    {
+        dialog.dialogRunner.Add(yarnDialog);
+    }
+    public void beginDialog(){
+        dialog.dialogRunner.StartDialogue(yarnStartNode);
+    }
+}
