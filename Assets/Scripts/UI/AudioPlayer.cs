1aacfe68d8dab59bfb85089208b26d896e994c9e
Slashscreen <SlashScreen@users.noreply.github.com>
2020-12-29 13:09:44 -0800

    reorganized, added "shader" although it needs help

---
+++ Assets/Scripts/UI/AudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [YarnCommand("PlaySFX")]
+    IEnumerator playSFX(string[] clipSource){
+        //play SFX and wait until done
+        AudioClip clip = Resources.Load<AudioClip>("Audio/"+clipSource[0]+".ogg");
+        gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
+        yield return new WaitUntil(() => GetComponent<AudioSource>().isPlaying == false);
+    }
+
+    [YarnCommand("StartSFX")]
+    void startSFX(string[] clipSource){
+        //Play SFX but not wait until done
+        AudioClip clip = Resources.Load<AudioClip>("Audio/"+clipSource[0]+".ogg");
+        gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
+    }
+
+}
---
+++ Assets/Scripts/UI/MusicScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using Yarn.Unity;
+
+public class MusicScript : MonoBehaviour
+{
+    public AudioClip music;
+
+    private void Start()
+    {
+        gameObject.GetComponent<AudioSource>().clip = music;
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    [YarnCommand("PlayMusic")]
+    void playMusic(string[] clipSource){
+        gameObject.GetComponent<AudioSource>().Pause();
+        AudioClip clip = Resources.Load<AudioClip>("Music/"+clipSource[0]+".ogg");
+        StartCoroutine(startMusic(clip));
+    }
+
+    IEnumerator startMusic(AudioClip clip){
+        //Play the track, then continue
+        gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
+        yield return new WaitUntil(() => GetComponent<AudioSource>().isPlaying == false);
+        gameObject.GetComponent<AudioSource>().UnPause();
+    }
+
+    
+}
