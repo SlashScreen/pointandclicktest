943ed06e8e9c8075114e86c0078fb17c21a7b19e
Slashscreen <SlashScreen@users.noreply.github.com>
2021-01-04 00:02:43 -0800

    added props and shoe

---
+++ Assets/Scripts/Misc/LivingRoomBackdrop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingRoomBackdrop : MonoBehaviour
+{
+    public Sprite noshoes;
+    private void Start()
+    {
+        GameObject.Find("Dialogue").GetComponent<DialogControllerComponent>().dia.AddCommandHandler("removeShoesLivingRoom",removeShoes);
+    }
+    public void removeShoes(string[] s){
+        Debug.Log("shoes removed");
+        GetComponent<SpriteRenderer>().sprite = noshoes;
+    }
+}
