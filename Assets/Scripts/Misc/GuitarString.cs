8910a21d10c53a739b7bcc88a57137f1aefa3411
Slashscreen <SlashScreen@users.noreply.github.com>
2021-01-13 00:37:02 -0800

    added guitar strings

---
+++ Assets/Scripts/Misc/GuitarString.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuitarString : MonoBehaviour
+{
+    public float amp = 1;
+    public float decay = .5f;
+    SineWave s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<SineWave>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        amp -= (amp * decay) * Time.deltaTime; //calculate decay
+        s.amplitude = amp;
+        s.reversed = !s.reversed; //flip directions
+    }
+}
---
+++ Assets/Scripts/Misc/SineWave.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineWave : MonoBehaviour
+{
+    public int points = 10;
+    public float length = 5;
+    public float amplitude;
+    public bool reversed;
+    LineRenderer line;
+    Vector3[] pts;
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        pts = new Vector3[points];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < pts.Length; i++){
+            pts[i].x = transform.position.x + ((length/length)*i); //set x
+            if(!reversed){ //vibrate string 
+                pts[i].y = Mathf.Sin(pts[i].x)*amplitude;
+            }else{
+                pts[i].y = Mathf.Sin(pts[i].x)*amplitude*-1;
+            }
+            
+            pts[i].z = transform.position.z;
+        }
+
+        line.SetPositions(pts);
+    }
+}
