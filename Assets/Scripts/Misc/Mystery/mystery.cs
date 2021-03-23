ea30cc50b3de8be1be4f80675ba5cf2d1426246f
Slashscreen <SlashScreen@users.noreply.github.com>
2021-03-22 22:21:20 -0700

    added kit violin voice (needs work), and also created the mystery solving system

---
+++ Assets/Scripts/Misc/Mystery/mystery.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class mystery : MonoBehaviour
+{
+    public string answer;
+    public string successNode;
+    public string failureNode;
+    public Dropdown[] drops;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (GameObject.Find("Player")){
+            GameObject.Find("Player").GetComponent<PlayerMain>().d.dia.AddFunction("AddOption",2,AddOption);
+        }
+    }
+
+    void AddOption(Yarn.Value[] vals){
+        //adds an option to the dropdown
+        drops[Mathf.RoundToInt(vals[0].AsNumber)].options.Add(new Dropdown.OptionData(vals[1].AsString));
+    }
+
+    string getDropdownText(int which){
+        return drops[which].options[drops[which].value].text;
+    }
+
+    [YarnCommand("Hide")]
+    public void Hide(){
+        gameObject.SetActive(false);
+    }
+
+    [YarnCommand("Show")]
+    public void Show(){
+        gameObject.SetActive(true);
+    }
+
+    public void recalculate(){
+        string res = "It was " + getDropdownText(0) + " in " + getDropdownText(1) + " with " + getDropdownText(2) + " because " + getDropdownText(3);
+        
+        if (res.ToLower() == answer.ToLower()){
+            Debug.Log("you guessed it!");
+            if (GameObject.Find("Player")){
+                GameObject.Find("Player").GetComponent<PlayerMain>().d.dia.StartDialogue(successNode);
+            }
+        }else{
+            if (GameObject.Find("Player")){ //inefficient but like. nobody playing this is going to be using a pentium 2 and this isn't called every frame. so. 
+                GameObject.Find("Player").GetComponent<PlayerMain>().d.dia.StartDialogue(failureNode);
+            }
+        }
+    }
+}
