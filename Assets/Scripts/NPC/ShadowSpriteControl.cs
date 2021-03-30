38ecc48959b9526b50af4c8187ddcee4e31301c2
Slashscreen <SlashScreen@users.noreply.github.com>
2021-03-29 22:56:30 -0700

    added shadow mark 1

---
+++ Assets/Scripts/NPC/ShadowSpriteControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowSpriteControl : MonoBehaviour
+{
+    public SpriteRenderer parentRenderer;
+    SpriteRenderer rend;
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        rend.sprite = parentRenderer.sprite;
+        transform.localScale = new Vector2(parentRenderer.transform.localScale.x,transform.localScale.y);
+    }
+}
