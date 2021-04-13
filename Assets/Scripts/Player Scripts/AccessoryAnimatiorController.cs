ad1d548cf95ba4a096239e6fc71b4568c0f0a27f
Slashscreen <SlashScreen@users.noreply.github.com>
2021-04-13 13:07:39 -0700

    redid zero's animations to fix desync between shoes and body

--- Assets/Scripts/Player Scripts/AccessoryAnimatiorController.cs
+++ Assets/Scripts/Player Scripts/AccessoryAnimatiorController.cs
@@ -20,12 +20,12 @@ public class AccessoryAnimatiorController : MonoBehaviour
 
     void Update()
     {
-        accessoryAnimator.SetInteger("direction", playerAnimator.GetInteger("direction"));
+        /*accessoryAnimator.SetInteger("direction", playerAnimator.GetInteger("direction"));
         accessoryAnimator.SetBool("facing_left", playerAnimator.GetBool("facing_left"));
         accessoryAnimator.SetBool("walking", playerAnimator.GetBool("walking"));
         accessoryAnimator.SetInteger("animation", playerAnimator.GetInteger("animation"));
         transform.localScale = playerAnimator.gameObject.transform.localScale;
-        transform.position = playerAnimator.gameObject.transform.position + new Vector3(0,0,-0.1f);// + offset;
+        transform.position = playerAnimator.gameObject.transform.position + new Vector3(0,0,-0.1f);// + offset;*/
         r.enabled = !hidden; //dont like calling this every frame but its not a huge issue
     }
 
